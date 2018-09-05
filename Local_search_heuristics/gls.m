0fbd9821c54570d9eaa70a0f71236464ff82bd12
kstoeckl <kurt.h.stoeckl@gmail.com>
2018-09-05 13:35:06 +1000

    function name change missed

--- Local_search_heuristics/gls.m
+++ Local_search_heuristics/gls.m
@@ -25,7 +25,7 @@
     % num_exchanges:
         % number of (k-)exchanges performed
 function [output_array, makespan, num_exchanges] = ...
-                            ms_solver_gls_v2(inputArray, k_exch, init_algo)
+                            gls(inputArray, k_exch, init_algo)
 
 % Variable checking
 allowed_init_algos = ["simple", "random", "naive"];
