704a6e6804f4f566a353acd0b6a26d190d164ac1
kstoeckl <kurt.h.stoeckl@gmail.com>
2018-10-08 16:18:41 +1100

    Some pseudo for a possible crossover

--- README.md
+++ README.md
@@ -3,11 +3,8 @@ A group project, exploring heuristics for solving the Minimum Makespan
 Scheduling Problem.
 
 # TODO
-* Genetic alg v2 appears to get stuck when run on hard test case here,
-shuffle_elmts_pairs
-...
-while isempty(m2_elements)
-    machines_shuffled(2) = randsample([1:(machines_shuffled(1)-1),...
-    (machines_shuffled(1)+1):num_machines],1);
-    m2_elements = find(gene_array == machines_shuffled(2));
-end
+* Measuring empirical performance of algs might be worth looking into
+https://theory.stanford.edu/~aiken/publications/papers/fse07.pdf
+* So many bad possible instances just moving randomly unlikely to do anything
+so idealy would have a less random system for mutation and or crossover.
+* 
---
+++ Reference_code/cross_over.py
@@ -0,0 +1,35 @@
+
+
+def cross_over1(p1, p2, num_jobs, num_machines):
+    un_assigned = 1:num_jobs
+
+    machines_p1 = shuffle(p1.machines)
+    machines_p2 = shuffle(p2.machines)
+
+    p1i = 1
+    p2i = 1
+
+    current_p = 1
+
+    child = {}
+    child_i = 1
+
+    while p1i<=num_machines || p2i <= num_machines:
+        if current_p == 1:
+            while p1i<=num_machines && machines_p1(p1i) not in un_assigned:
+                p1i += 1
+
+            child.machines(child_i) = machines_p1(p1i)
+            un_assigned = un_assigned\child.machines(child_i)
+            current_p = 2
+
+        if current_p == 2:
+            while p2i<=num_machines && machines_p1(p2i) not in un_assigned:
+                p2i += 1
+
+            child.machines(child_i) = machines_p2(p2i)
+            un_assigned = un_assigned\child.machines(child_i)
+            current_p = 1
+
+    use greedy to assign rest of un_assigned to child
+    return child
